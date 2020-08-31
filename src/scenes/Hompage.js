aff79d818eeb50d4830d1c794fdba262718aac8e
badriysha <badriysha@gmail.com>
2020-08-31 07:06:50 +0700

    firts commit

---
+++ src/scenes/Hompage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "elements/Button";
+
+export default function Hompage() {
+  return (
+    <div>
+      <Button isPrimary isSmall>
+        Sign Up
+      </Button>
+    </div>
+  );
+}
